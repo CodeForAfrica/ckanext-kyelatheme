19d5cd246db52318643dcc359ab9b2836a15771a
Joseph Luvanda <lvandazz@gmai.com>
2017-10-02 18:19:20 +0300

    Add spatial field on dataset schema

--- ckanext/kyelatheme/plugin.py
+++ ckanext/kyelatheme/plugin.py
@@ -2,8 +2,9 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class KyelathemePlugin(plugins.SingletonPlugin):
+class KyelathemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)
 
     # IConfigurer
 
@@ -11,3 +12,43 @@ class KyelathemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'kyelatheme')
+
+
+    def create_package_schema(self):
+        #Default schema
+        schema = super(KyelathemePlugin, self).create_package_schema()
+        #Add 'spatial' field
+        schema.update({
+            'spatial': [toolkit.get_validator('ignore_missing'),
+                        toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def update_package_schema(self):
+        #Default schema
+        schema = super(KyelathemePlugin, self).update_package_schema()
+        #Update 'spatial' field
+        schema.update({
+            'spatial': [toolkit.get_validator('ignore_missing'),
+                        toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+
+    def show_package_schema(self):
+        #Default schema
+        schema = super(KyelathemePlugin, self).show_package_schema()
+        schema.update({
+            'spatial': [toolkit.get_converter('convert_from_extras'),
+            toolkit.get_validator('ignore_missing')]
+        })
+        return schema
+
+
+    def is_fallback(self):
+        return True
+
+
+    def package_types(self):
+        #Handles no specific types
+        return []
--- ckanext/kyelatheme/public/bananas.js
+++ ckanext/kyelatheme/public/bananas.js
@@ -1,3 +1,51 @@
-$(function(){
-  $('.dropdown-toggle').dropdown();
+var obj ;
+$(document).ready(function(){
+
+  try {
+
+    $('.dropdown-toggle').dropdown();
+
+
+    /**
+    * Dataset 'spatial' field for ckanext-spatial
+    **/
+    var spatialInput = $('#field-spatial');
+
+    //spatialInput.attr('readonly', true);
+    // Leaflet Map
+    var theMap = L.map('mapid').setView([-8.9058427,33.4223479], 8);
+    //Add tiles layer
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  		maxZoom: 18,
+  		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  		id: 'mapbox.streets'
+  	}).addTo(theMap);
+
+    //set marker to map center
+    var theMarker = L.marker([-8.9058427,33.4223479],{draggable:true}).addTo(theMap);
+
+    theMarker.on('dragend', function(event){
+        var marker = event.target;
+        var position = marker.getLatLng();
+        //console.log(position);
+        marker.setLatLng(position,{draggable:'true'}).bindPopup(position).update();
+        spatialInput.attr('value', "{\"type\":\"Point\", \"coordinates\":["+this.toGeoJSON().geometry.coordinates+"]}");
+        console.log(this.toGeoJSON().geometry);
+    });
+
+    //Map Events
+     theMap.on('click', onMapClick);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+
+  }
+
 });
+
+  function onMapClick(e) {
+    console.log(e);
+  }
